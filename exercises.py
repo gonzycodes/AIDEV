78273ea331d5b68e209a857d2c14033216d0b2a5
Nahuel Gonzalez Jansson <nahuel.gonzalezj@hotmail.comgit config --global user.name Nahuel>
2025-09-09 13:51:34 +0200

    Completed more exercises

--- exercises.py
+++ exercises.py
@@ -9,3 +9,27 @@ def ex_1_2():
     return "busted" if total > 21 else "safe"
     raise NotImplementedError("Exercise 1.2 not implemented")
 
+def ex_1_3():
+    card1, card2, card3 = 4, 2, 7
+    total = card1 + card2 + card3
+    if total < 21: 
+     return "Safe"
+    elif total > 21:
+     return "Busted"
+    else:
+     return "Blackjack"
+    raise NotImplementedError("Exercise 1.3 not implemented")
+
+def ex_1_4():
+    dealer1, dealer2, dealer3 = 1, 6, 7
+    result = dealer1 + dealer2 + dealer3
+    if result < 17:
+     return "Pick"
+    elif 17 <= result < 21:
+     return "Stop"
+    elif result == 21:
+     return "Blackjack"
+    else:
+     return "Busted"
+    
+    raise NotImplementedError("Exercise 1.4 not implemented")
