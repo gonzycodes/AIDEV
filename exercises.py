3305d29c59e0c3c05e9184c4762f9f8a40b00466
Nahuel Gonzalez Jansson <nahuel.gonzalezj@hotmail.comgit config --global user.name Nahuel>
2025-09-09 13:15:23 +0200

    Started on loops and if statements exercises

---
+++ exercises.py
@@ -0,0 +1,11 @@
+def ex_1_1():
+    card1, card2, card3, card4, card5 = 4, 2, 7, 1, 11
+    result = card1 + card2 + card3 + card4 + card5
+    return result
+    
+def ex_1_2():
+    card1, card2, card3, card4, card5 = 4, 2, 7, 1, 11
+    total = card1 + card2 + card3 + card4 + card5
+    return "busted" if total > 21 else "safe"
+    raise NotImplementedError("Exercise 1.2 not implemented")
+
